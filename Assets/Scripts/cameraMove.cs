00c13a01f1aee1d07da9492431cab80f9fd7543d
Sayuri <colorpencil0312@gmail.com>
2014-10-08 23:16:46 +0900

    指でスクロールできるように ビルドできなくなった（環境変数の設定をしないといけない？）

---
+++ Assets/Scripts/cameraMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMove : MonoBehaviour {
+
+	public float moveSpeed = 0.1f;
+	public float cameraZ;
+
+	// Use this for initialization
+	void Start () {
+		cameraZ = this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//transform.position = new Vector3(0, Mathf.Clamp(transform.position.y,0,-47.40752f), cameraZ);
+
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			transform.Translate(0, -touchDeltaPosition.y * moveSpeed, 0);
+
+			/*Vector3 cameraPos = transform.position;
+			cameraPos.y = Mathf.Clamp (cameraPos.y, -8.0f, -47.40752f);
+			transform.position = cameraPos;*/
+
+
+		}
+	}
+
+}

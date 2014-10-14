982c1cb391ddf194c086ec29553c11c41692563c
Sayuri <colorpencil0312@gmail.com>
2014-10-14 14:07:39 +0900

    急にビルドが出来るように…再起動のおかげ？

--- Assets/Scripts/cameraMove.cs
+++ Assets/Scripts/cameraMove.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class cameraMove : MonoBehaviour {
 
-	public float moveSpeed = 0.1f;
+	public float moveSpeed = 0.08f;
 	public float cameraZ;
 
 	// Use this for initialization
@@ -14,19 +14,28 @@ public class cameraMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		//transform.position = new Vector3(0, Mathf.Clamp(transform.position.y,0,-47.40752f), cameraZ);
+		//Vector3 cameraPos = transform.position;
+		//cameraPos.y = Mathf.Clamp (cameraPos.y, -8.0f, -47.40752f);
 
-		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
 
+		if (Input.touchCount >0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+
+			if(this.transform.position.z == cameraZ){
+			//Vector3 nowPos = this.transform.position;
 			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 			transform.Translate(0, -touchDeltaPosition.y * moveSpeed, 0);
+			}
 
-			/*Vector3 cameraPos = transform.position;
-			cameraPos.y = Mathf.Clamp (cameraPos.y, -8.0f, -47.40752f);
-			transform.position = cameraPos;*/
+			else{
+				if (Input.touchCount ==1 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+					Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+					transform.Translate(-touchDeltaPosition.x * moveSpeed, -touchDeltaPosition.y * moveSpeed, 0);
+				}
+			}
 
 
 		}
+
 	}
 
 }
---
+++ Assets/Scripts/camera_pinch.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_pinch : MonoBehaviour {
+	public float zMin = -100.0f;
+	public float zMax = 25.0f;
+	public float pinchRate = 1.0f;
+
+	private float backDist = 0.0f;
+
+	void Update(){
+		if (Input.touchCount == 2) {
+			Touch t1 = Input.GetTouch(0);
+			Touch t2 = Input.GetTouch(1);
+			//2点タッチ開始時の距離を記憶
+			if(t2.phase == TouchPhase.Began){
+				backDist = Vector2.Distance(t1.position, t2.position);
+				Debug.Log ("Touch");
+			}
+			//ピンチイン・アウト
+			else if (t1.phase == TouchPhase.Moved && t2.phase == TouchPhase.Moved){
+				float newDist = Vector2.Distance(t1.position, t2.position);
+				float transZ = (newDist - backDist) / pinchRate;
+				Debug.Log ("Pinch");
+
+				if(transZ != 0){
+
+
+					Vector3 newPos = this.transform.position;
+					newPos.z += transZ;
+
+					//zMin～zMaxの範囲内のみ.
+					if(zMin <= newPos.z && newPos.z <= zMax){
+						Debug.Log ("newPos.z=" + newPos.z);
+						this.transform.position = newPos;
+						backDist = newDist;
+					}
+				}
+
+			}
+		}
+	}
+}

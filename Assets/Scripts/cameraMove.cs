b564f054024774f7b63d09ce14cd1a66dd19d864
Sayuri <colorpencil0312@gmail.com>
2014-10-16 16:54:14 +0900

    カメラのMath.f Clamp解決（最小値と最大値が逆）

--- Assets/Scripts/cameraMove.cs
+++ Assets/Scripts/cameraMove.cs
@@ -9,29 +9,41 @@ public class cameraMove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		cameraZ = this.transform.position.z;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//Vector3 cameraPos = transform.position;
-		//cameraPos.y = Mathf.Clamp (cameraPos.y, -8.0f, -47.40752f);
+
+		Vector3 cameraPos = transform.position;
+		cameraPos.y = Mathf.Clamp (cameraPos.y, -8.0f, -47.40752f);
+
+		//transform.position.y = Mathf.Clamp (transform.position.y, -4, 4);
+		//transform.position.x = Mathf.Clamp(transform.position.x,-10,10);
+		/*if (cameraPos.y < ) {
+
+		}*/
 
 
 		if (Input.touchCount >0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
 
-			if(this.transform.position.z == cameraZ){
-			//Vector3 nowPos = this.transform.position;
-			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-			transform.Translate(0, -touchDeltaPosition.y * moveSpeed, 0);
+			/*if(this.transform.position.z == cameraZ){
+				//Vector3 nowPos = this.transform.position;
+				Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+				transform.Translate(-touchDeltaPosition.x * moveSpeed, -touchDeltaPosition.y * moveSpeed, 0);
 			}
 
-			else{
+			else
+			{*/
 				if (Input.touchCount ==1 && Input.GetTouch (0).phase == TouchPhase.Moved) {
 					Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 					transform.Translate(-touchDeltaPosition.x * moveSpeed, -touchDeltaPosition.y * moveSpeed, 0);
-				}
+					
+					float limit_value = Mathf.Clamp (transform.position.y, -47.40752f, -8.0f);
+					transform.position = new Vector3 (transform.position.x, limit_value, transform.position.z);
 			}
+			//}
 
 
 		}

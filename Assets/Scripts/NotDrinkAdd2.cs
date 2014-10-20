8058cbb587efb2db28eb262454b746f140eb466f
Sayuri <colorpencil0312@gmail.com>
2014-10-20 11:05:40 +0900

    NotDrink3シーンで正方形の４コマを表示できるように

--- Assets/Scripts/NotDrinkAdd2.cs
+++ Assets/Scripts/NotDrinkAdd2.cs
@@ -15,6 +15,7 @@ public class NotDrinkAdd2 : MonoBehaviour {
 	public GameObject coma9;
 	public GameObject coma10;
 	public float cameraZ;
+	public float cameraX;
 	public float cameraDif = -5.2f;
 	
 	public int maxComa = 10;
@@ -22,7 +23,8 @@ public class NotDrinkAdd2 : MonoBehaviour {
 	GameObject MainCam;	
 	
 	void Start () {
-		
+
+		cameraX = this.transform.position.x;
 		cameraZ = this.transform.position.z;
 
 		comaNum = 0;
@@ -39,47 +41,47 @@ public class NotDrinkAdd2 : MonoBehaviour {
 
 			if(comaNum == 1){
 				Instantiate(coma2);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 2){
 				Instantiate(coma3);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 3){
 				Instantiate(coma4);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 4){
 				Instantiate(coma5);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 5){
 				Instantiate(coma6);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 6){
 				Instantiate(coma7);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 7){
 				Instantiate(coma8);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 8){
 				Instantiate(coma9);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			if(comaNum == 9){
 				Instantiate(coma10);
-				transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+				transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 				Debug.Log(comaNum);
 			}
 			
@@ -96,47 +98,47 @@ public class NotDrinkAdd2 : MonoBehaviour {
 			
 				if(comaNum == 1){
 					Instantiate(coma2);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 2){
 					Instantiate(coma3);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 3){
 					Instantiate(coma4);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 4){
 					Instantiate(coma5);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 5){
 					Instantiate(coma6);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 6){
 					Instantiate(coma7);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 7){
 					Instantiate(coma8);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 8){
 					Instantiate(coma9);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 				if(comaNum == 9){
 					Instantiate(coma10);
-					transform.position = new Vector3(0, cameraDif*comaNum, cameraZ);
+					transform.position = new Vector3(cameraX, cameraDif*comaNum, cameraZ);
 					Debug.Log(comaNum);
 				}
 			}

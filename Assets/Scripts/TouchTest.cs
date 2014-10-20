36427e2e3abcfe2775487ca65d9de14f2a14ff42
Sayuri <colorpencil0312@gmail.com>
2014-10-20 10:43:23 +0900

    komaTest用のシーンを作る（60mm×60mm）

--- Assets/Scripts/TouchTest.cs
+++ Assets/Scripts/TouchTest.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class TouchTest : MonoBehaviour {
 
+	public float camRayLength = 100f;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	void Update () {
+	/*void Update () {
 		if  (Input.touchCount == 1){
 			
 			Vector3    aTapPoint   = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -17,6 +19,32 @@ public class TouchTest : MonoBehaviour {
 			if (aCollider2d) {
 				GameObject obj = aCollider2d.transform.gameObject;
 				Debug.Log(obj.name);
+			}
+		}
+	}*/
+
+	 /*if(Input.GetKeyUp(KeyCode.Space)){
+		Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+		if(collition2d){
+			RaycastHit2D hitObject = Physics2D.Raycast(tapPoint,-Vector2.up);
+			if (hitObject) {
+				Debug.Log("hit object is " + hitObject.collider.gameObject.name);
+			}
+		}
+	}*/
+
+	void Update(){
+
+		if(Input.GetKeyUp(KeyCode.Space)){
+			Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+	
+			RaycastHit floorHit;
+	
+			if(Physics.Raycast (camRay, out floorHit, camRayLength))
+			{
+				//GameObject obj = floorHit.transform.gameObject;
+				Debug.Log("Touch");
 			}
 		}
 	}

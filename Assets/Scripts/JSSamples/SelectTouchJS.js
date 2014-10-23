b31533b1d9b897816b026d8cedd865903e78ec34
Sayuri <colorpencil0312@gmail.com>
2014-10-23 16:50:23 +0900

    タッチでオブジェクトを選べるコードをC#に書き直す

---
+++ Assets/Scripts/JSSamples/SelectTouchJS.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var coma1 : GameObject;
+
+function Start () {
+
+}
+
+function Update () { 
+	if ( Input.touchCount == 1) {
+    var tapPoint : Vector2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    var collition2d : Collider2D = Physics2D.OverlapPoint(tapPoint);
+    	
+    	if (collition2d) {
+        var hitObject : RaycastHit2D = Physics2D.Raycast(tapPoint,-Vector2.up);
+     		
+     		if (hitObject) {
+            	Debug.Log("hit object is " + hitObject.collider.gameObject.name);
+            	if(hitObject.collider.gameObject.name == "4komaTest_66_1"){
+            		Debug.Log("Hit");
+            		Destroy(coma1);
+            	}
+        	}
+    	}
+}
+
+
+}
---
+++ Assets/Scripts/SelectTouch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectTouch : MonoBehaviour {
+
+	public GameObject coma1;
+
+	void Update(){
+		if ( Input.touchCount == 1) {
+			Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collition2d  = Physics2D.OverlapPoint(tapPoint);
+			
+			if (collition2d) {
+				RaycastHit2D hitObject = Physics2D.Raycast(tapPoint,-Vector2.up);
+				
+				if (hitObject) {
+					Debug.Log("hit object is " + hitObject.collider.gameObject.name);
+					if(hitObject.collider.gameObject.name == "4komaTest_66_1"){
+						Debug.Log("Hit");
+						Destroy(coma1);
+					}
+				}
+			}
+		}
+
+	}
+}

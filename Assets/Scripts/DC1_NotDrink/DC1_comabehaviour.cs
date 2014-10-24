ab54027e6e3c271fc661b0c00ad90caa99136fa7
Sayuri <colorpencil0312@gmail.com>
2014-10-25 00:10:24 +0900

    DC1_comabehaviour.csを作成。コマのスクロールと、１コマ目のy座標がが5.3以上になると2コマ目にピタッと移動するように

---
+++ Assets/Scripts/DC1_NotDrink/DC1_comabehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DC1_comabehaviour : MonoBehaviour {
+	public float moveSpeed = 0.08f;
+	public GameObject comaFirst;
+
+	float comaFirstZ;
+	float comaFirstX;
+
+	// Use this for initialization
+	void Start () {
+		comaFirstX = comaFirst.transform.position.x;
+		comaFirstZ = comaFirst.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount  ==1 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			transform.Translate(0, touchDeltaPosition.y * moveSpeed, 0);
+			
+			float limit_valueY = Mathf.Clamp (transform.position.y,  0.0f, 21.5f);
+			//float limit_valueX = Mathf.Clamp (transform.position.x, -2.7f, 3.0f);
+			transform.position = new Vector3 (0, limit_valueY, transform.position.z);
+			
+			
+		}
+
+		if(Input.touchCount  ==1 && Input.GetTouch (0).phase == TouchPhase.Ended){
+			if(comaFirst.transform.position.y > 5.3f){
+				comaFirst.transform.position = new Vector3(comaFirstX, 7.9f, comaFirstZ);
+			}
+
+		}
+	}
+}

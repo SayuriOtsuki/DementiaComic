1871547b3ed327f7372fae1003bdec0c34a0ab99
Sayuri <colorpencil0312@gmail.com>
2014-10-24 23:46:48 +0900

    Y軸のスクロールを修正（逆に）

--- Assets/Scripts/TouchMove.cs
+++ Assets/Scripts/TouchMove.cs
@@ -14,7 +14,7 @@ public class TouchMove : MonoBehaviour {
 		if (Input.touchCount  ==1 && Input.GetTouch (0).phase == TouchPhase.Moved) {
 			
 			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-			transform.Translate(-touchDeltaPosition.x * moveSpeed, -touchDeltaPosition.y * moveSpeed, 0);
+			transform.Translate(0, touchDeltaPosition.y * moveSpeed, 0);
 			
 			float limit_valueY = Mathf.Clamp (transform.position.y,  0.0f, 21.5f);
 			//float limit_valueX = Mathf.Clamp (transform.position.x, -2.7f, 3.0f);

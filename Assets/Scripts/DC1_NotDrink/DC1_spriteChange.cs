0d04cd0c1b11d8e34c76905cbd0ce815488ccf27
Sayuri <colorpencil0312@gmail.com>
2014-10-25 01:12:46 +0900

    指で触っているオブジェクトを判定できるように。spriteが変えられない→spriteRenderer.sprite = Resources.Load<Sprite>("DC1_04");

---
+++ Assets/Scripts/DC1_NotDrink/DC1_spriteChange.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DC1_spriteChange : MonoBehaviour {
+
+	public GameObject coma3;
+	public SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+	
+		spriteRenderer = coma3.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 1) {
+			Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collition2d  = Physics2D.OverlapPoint(tapPoint);
+			
+			if (collition2d) {
+				RaycastHit2D hitObject = Physics2D.Raycast(tapPoint,-Vector2.up);
+				
+				if (hitObject) {
+					Debug.Log("hit object is " + hitObject.collider.gameObject.name);
+					if(hitObject.collider.gameObject.name == "4komaTest_66_3"){
+						Debug.Log("Hit");
+						spriteRenderer.sprite = Resources.Load<Sprite>("DC1_04");
+					}
+				}
+			}
+		}
+	}
+}
